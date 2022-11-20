5193f8cd5ed35e0869b22cd8f1cd303b1118e906
Maks <alisa3d2007@gmail.com>
2022-11-20 18:54:04 +0300

    added post.service

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,59 +1,36 @@
 import { Body, Controller, Get, Param, Post, Delete, Put } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { PostService } from './post.service';
 
 @Controller('post')
 export class PostController {
   posts: any[];
 
-  constructor() {
-    this.posts = [
-      {
-        id: 1,
-        content: 'test text1',
-      },
-      {
-        id: 2,
-        content: 'test text2',
-      },
-    ];
-  }
+  constructor(private readonly postService: PostService) { }
 
   @Get()
   async getAll() {
-    return this.posts;
+    return this.postService.getAll();
   }
 
   @Get(':id')
   async getById(@Param('id') id: string) {
-    return this.posts.find((post) => post.id == id);
+    return this.postService.getById(id);
   }
 
   @Put(':id')
   async update(@Param('id') id: string, @Body() dto: UpdatePostDto) {
-    const newPosts = this.posts.map((post) => {
-      if (post.id == id) {
-        return dto;
-      } else {
-        return post;
-      }
-    });
-
-    this.posts = newPosts;
-    return dto;
+    return this.postService.update(id, dto);
   }
 
   @Delete(':id')
   async removePost(@Param('id') id: string) {
-    const newPosts = this.posts.filter((post) => post.id != id);
-    this.posts = newPosts;
-    return this.posts;
+    return this.postService.removePost(id);
   }
 
   @Post()
   async createPost(@Body() dto: CreatePostDto) {
-    const newPosts = [...this.posts, dto];
-    this.posts = newPosts;
-    return newPosts;
+    return this.postService.createPost(dto);
   }
 }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { CreatePostDto } from './dto/create-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  posts: any[];
+
+  constructor() {
+    this.posts = [
+      {
+        id: 1,
+        content: 'test text1',
+      },
+      {
+        id: 2,
+        content: 'test text2',
+      },
+    ];
+  }
+
+  async getAll() {
+    return this.posts;
+  }
+
+  async getById(id: string) {
+    return this.posts.find(post => post.id == id);
+  }
+
+  async update(id: string, dto: UpdatePostDto) {
+    const newPosts = this.posts.map((post) => {
+      if (post.id == id) {
+        return dto;
+      } else {
+        return post;
+      }
+    });
+
+    this.posts = newPosts;
+    return dto;
+  }
+
+  async removePost(id: string) {
+    const newPosts = this.posts.filter((post) => post.id != id);
+    this.posts = newPosts;
+    return this.posts;
+  }
+
+
+  async createPost(dto: CreatePostDto) {
+    const newPosts = [...this.posts, dto];
+    this.posts = newPosts;
+    return newPosts;
+  }
+}
