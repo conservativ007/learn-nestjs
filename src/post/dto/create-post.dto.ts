5f5de7c4fade64060f110a17a983c37998127c68
Maks <alisa3d2007@gmail.com>
2022-11-21 13:29:20 +0300

    added readme

--- src/post/dto/create-post.dto.ts
+++
@@ -1,4 +0,0 @@
-export class CreatePostDto {
-  readonly content: string;
-  readonly userName: string;
-}
--- src/post/dto/update-post.dto.ts
+++
@@ -1,5 +0,0 @@
-export class UpdatePostDto {
-  readonly id: string;
-  readonly content: string;
-  readonly userName: string;
-}
